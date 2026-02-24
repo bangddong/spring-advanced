cc67d77396c40c2940e735b8d89c05832ed7f17e
bangddong <gusehdqkd@naver.com>
2026-02-24 22:28:08 +0900

    feat : 로그 추적기 샘플 추가

---
+++ src/main/java/hello/advanced/app/v0/OrderRepositoryV0.java
@@ -0,0 +1,27 @@
+package hello.advanced.app.v0;
+
+import org.springframework.stereotype.Repository;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV0 {
+
+	public void save(String itemId) {
+		// 저장 로직
+		if (itemId.equals("ex")) {
+			throw new IllegalStateException("예외 발생!");
+		}
+		// 상품 저장시 1초가 걸린다고 가정
+		sleep(1000);
+	}
+
+	private void sleep(int millis) {
+		try {
+			Thread.sleep(millis);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
